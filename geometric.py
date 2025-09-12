08d6b0ae0264efed25dc71a0e2533b699e689f44
Alejandra <ahinostrozac@uni.pe>
2025-09-12 22:02:41 +0200

    add configurations to generate BoB descriptor

--- geometric.py
+++ geometric.py
@@ -8,20 +8,18 @@ from ase.io import read
 from qml.representations import generate_coulomb_matrix, generate_bob, generate_slatm, get_slatm_mbtypes
 from krr_opt import *
 
-# values for tox
+# FIXED CONFIGURATION FOR TDCOMMONS-LD50 AND MOLECULENET-LIPOPHILICITY DATABASES
 # max_atoms: The maximum number of atoms in the representation
 # asize: The maximum number of atoms of each element type supported by the representation
-max_atoms = 196
-asize_tox = {'C': 62, 'H': 111, 'N': 16, 'O': 35, 'S': 8, 'Cl': 12, 'F': 20, 'Br': 6, 'I':6, 'P': 4}
-atomtypes = dict(asize_tox.keys())
 
+tox_database_config = dict(max_atoms=196,
+                           asize={'C': 62, 'H': 111, 'N': 16, 'O': 35, 'S': 8, 'Cl': 12, 'F': 20, 'Br': 6, 'I':6, 'P': 4})
 
-def get_bob(filename, size=max_atoms, asize=asize_tox):
-    atoms = read(filename)
-    Z = atoms.get_atomic_numbers()
-    xyz = atoms.get_positions()
+lipo_database_config = dict(max_atoms=203,
+                            asize={'C': 66, 'H': 103, 'N': 17, 'O': 16, 'S': 3, 'Cl': 3, 'F': 9, 'Br': 2, 'I':3})
 
-    atomtypes = dict(asize.keys())
+def get_bob(Z, xyz, max_atoms, asize):
+    atomtypes = list(asize.keys())
     bob_repr = generate_bob(Z, xyz, 
                             size=max_atoms, 
                             atomtypes=atomtypes, 
